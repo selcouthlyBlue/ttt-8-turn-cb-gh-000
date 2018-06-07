185fe359dff73b3375dd8fe2eca8f8e11dbe682e
Jerome Patrick Gonzalvo <jvgonzalvo@up.edu.ph>
2018-06-07 05:58:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,7 @@
 def display_board(board)
-  
+  display_row(board, 0)
+end
+
+def display_row(board, row_index)
+  puts " #{board[3 * row_index]} | #{board[3 * row_index + 1]} | #{board[3 * row_index + 2]} "
 end
